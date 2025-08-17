81582690c5186d0df8ac5fe8f31eabdc904ee47b
Aditya kulkarni <aditya.amcec23@gmail.com>
2025-08-17 22:09:41 +0530

    update 3

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggleBtn = document.querySelector('.nav-toggle-btn');
+    const navList = document.querySelector('.nav-list');
+
+    navToggleBtn.addEventListener('click', () => {
+        navList.classList.toggle('nav-list-active');
+    });
+});
